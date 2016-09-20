c0e2766cfbdaeb808b1be71472909e8c78d6d9d6
whigy <whigy9@gmail.com>
2016-09-20 11:56:21 +0200

    Update rand.m

--- PattRecClasses/@HMM/rand.m
+++ PattRecClasses/@HMM/rand.m
@@ -1,1 +1,40 @@
-function [X,S]=rand(h,nSamples)%[X,S]=rand(h,nSamples);generates a random sequence of data%from a given Hidden Markov Model.%%Input:%h=         HMM object%nSamples=  maximum no of output samples (scalars or column vectors)%%Result:%X= matrix or row vector with output data samples%S= row vector with corresponding integer state values%   obtained from the h.StateGen component.%   nS= length(S) == size(X,2)= number of output samples.%   If the StateGen can generate infinite-duration sequences,%       nS == nSamples%   If the StateGen is a finite-duration MarkovChain,%       nS <= nSamples%%----------------------------------------------------%Code Authors:%----------------------------------------------------if numel(h)>1    error('Method works only for a single object');end;%continue coding from here, and delete the error message.% error('Not yet implemented');%h=HMM(mc,pD)%   creates a HMM with StateGen=mc, and OutputDistr=pDS = h.StateGen.rand(h.StateGen, nSamples);% Generate Statesif h.StateGen.finiteDuration()    S_true = S(1:end-1);else    S_true = S;endX = zeros(h.OutputDistr.DataSize, size(S_true,2));for i = 1 : size(S_true,2) % Generate Output    X(:, i) = h.OutputDistr(S_true(i)).rand(S_true(i),1);end
+function [X,S]=rand(h,nSamples)
+%[X,S]=rand(h,nSamples);generates a random sequence of data
+%from a given Hidden Markov Model.
+%
+%Input:
+%h=         HMM object
+%nSamples=  maximum no of output samples (scalars or column vectors)
+%
+%Result:
+%X= matrix or row vector with output data samples
+%S= row vector with corresponding integer state values
+%   obtained from the h.StateGen component.
+%   nS= length(S) == size(X,2)= number of output samples.
+%   If the StateGen can generate infinite-duration sequences,
+%       nS == nSamples
+%   If the StateGen is a finite-duration MarkovChain,
+%       nS <= nSamples
+%
+%----------------------------------------------------
+%Code Authors:
+%----------------------------------------------------
+
+if numel(h)>1
+    error('Method works only for a single object');
+end;
+
+%h=HMM(mc,pD)
+%   creates a HMM with StateGen=mc, and OutputDistr=pD
+
+S = rand(h.StateGen, nSamples);% Generate States
+if h.StateGen.finiteDuration()
+    S_true = S(1:end-1);
+else
+    S_true = S;
+end
+
+X = zeros(h.OutputDistr.DataSize, size(S_true,2));
+for i = 1 : size(S_true,2) % Generate Output
+    X(:, i) = rand(h.OutputDistr(S_true(i)),1);
+end
