34fd6bb21f186f429c9cb12d861643bfeb870295
rendanim <mbuvha.rendani@gmail.com>
2016-09-26 15:55:23 +0200

    added comments

--- PattRecClasses/Code_Verfication.m
+++ PattRecClasses/Code_Verfication.m
@@ -1,21 +1,23 @@
+%%  Set Randomseed
 
-%% Test MC%%
+ rng(1000);
+%%  Question 2 Test MC%%
 clear all;
 mc=MarkovChain([0.75;0.25], [0.99 0.01;0.03 0.97]);%State generator
 x=rand(mc, 10000); %Generate an output sequence
-s1=sum(x==1)/10000;
+s1=sum(x==1)/10000; %State Means
 s2=sum(x==2)/10000;
 
-%% Test HMM
+%%  Question 3 Test HMM
 g1=GaussD('Mean',0,'StDev',1) ;%Distribution for state=1
 g2=GaussD('Mean',3,'StDev',2) ;%Distribution for state=2
 h=HMM(mc, [g1; g2]); %The HMM
 
-[Y,S]=rand(h,10000);
+[Y,S]=rand(h,10000);% HMM Samples
 mU=mean(Y);
 vr=var(Y);
 
-%% Samples of X_t
+%% Question 4 500 Samples of X_t
 [X_t,S_t] =rand(h,500);
 X_t=reshape(X_t,1,500);
 t=1:500;
@@ -28,7 +30,7 @@ title('Samples from Hidden Markov Model')
 xlabel('Time Steps t')
 ylabel('Hidden Markov Emission (X_t)')
 
-%% Samples of X_t new distribution
+%% Question 5 Samples of X_t new distribution
 
 g11=GaussD('Mean',0,'StDev',1) ;%Distribution for state=1
 g22=GaussD('Mean',0,'StDev',2) ;%Distribution for state=2
@@ -45,8 +47,8 @@ title('Samples from Hidden Markov Model with equal Means')
 xlabel('Time Steps t')
 ylabel('Hidden Markov Emission (X_t)')
 
-%% Finite Duration
-clear all;
+%% Question 6 Finite Duration
+%clear all;
 mcf=MarkovChain([0.75;0.25], [0.99 0.01 0;0.02 0.97 0.01]);
 gf1=GaussD('Mean',0,'StDev',1) ;%Distribution for state=1
 gf2=GaussD('Mean',3,'StDev',2) ;%Distribution for state=2
@@ -58,23 +60,25 @@ mUf=mean(Yf);
 vrf=var(Yf);
 t=1:size(Sf,2);
 t1=1:(size(Sf,2)-1);
-plot(t,Sf,'--o');
+plot(t,Sf,'r-*');
 title('State samples over time')
 xlabel('Time Steps t')
 ylabel('Hidden Markov State ')
 ax=gca;
-ax.YTick=[0 1 2 3 4];
-figure()
+%ax.YTick=[0 1 2 3 4];
+hold on
 plot(t1,Yf);
 title('Samples from Hidden Markov Model of Finite Duration')
 xlabel('Time Steps t')
 ylabel('Hidden Markov Emission (X_t)')
+legend('State','Output')
+hold off
 
-%% Random Vector Generation
+%% Question 7 Random Vector Generation
 clear all;
 mcv=MarkovChain([0.75;0.25], [0.99 0.01;0.03 0.97]);
-g1v=GaussD('Mean',[4; 5],'Covariance',[1, 0;0, 1]);%Distribution for state=1
-g2v=GaussD('Mean',[1; 3],'Covariance',[2, 1;1, 4]); 
+g1v=GaussD('Mean',[4; 5],'Covariance',[1, 0;0, 1]); % State 1 distribution
+g2v=GaussD('Mean',[1; 3],'Covariance',[2, 1;1, 4]); % State 2 distribution
 hv=HMM(mcv, [g1v; g2v]);
 [Yv,Sv]=rand(hv,10000);
 
